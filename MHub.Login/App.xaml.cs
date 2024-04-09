ebe4f353835f2e6a10dab4042d8a0209c50966e1
Biniszer <kbiniszewski86@gmail.com>
2024-04-09 18:46:12 +0200

    created login page

---
+++ MHub.Login/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MHub.Login
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MHub.Login/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MHub.Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+ public partial class MainWindow : Window
+    {
+    public MainWindow()
+        {
+            InitializeUI();
+        }
+
+    private void LoginButton_Click() => InitializeUI();
+
+    private void InitializeUI()
+        {
+            InitializeComponent();
+            // Add any additional UI initialization logic here
+        }
+    }
+}
